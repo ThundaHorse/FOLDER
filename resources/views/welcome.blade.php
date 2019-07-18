04207fae65eb888254530e554e5647694fa3ae13
ThundaHorse <abeeeeekim@gmail.com>
2019-07-18 15:01:25 -0400

    laravel test project, blade template rendering

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">The beautiful Laravel</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Learning Laravel</h1>
+            <p>
+                This blog post will get you right on track with Laravel!
+            </p>
+            <p><a href='#'>Read more</a></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">The next Steps</h1>
+            <p>
+                Understanding the Basics is great, but you need to be able to take the next steps.
+            </p>
+            <p><a href="#">Read more</a></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Laravel 5.8</h1>
+            <p>
+                Though announced as a 'minor release', Laravel 5.8 ships with some very interesting additions and features.
+            </p>
+            <p><a href='#'>Read More</a></p>
+        </div>
+    </div>
+
+</div>
+@endsection
